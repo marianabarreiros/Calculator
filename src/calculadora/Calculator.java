c81fe514f9454d88bdbc48b5a5229268f6b502a5
Mariana Barreiros <maridepp1@hotmail.com>
2018-03-13 11:50:19 -0300

    criação dos métodos operadores

--- src/calculadora/Calculator.java
+++ src/calculadora/Calculator.java
@@ -14,9 +14,38 @@ public class Calculator extends javax.swing.JFrame {
     /**
      * Creates new form CalculatorJFrame
      */
+    
+    double num, ans;
+    int calculation;
+    
     public Calculator() {
         initComponents();
     }
+    
+    public void arithimetic_operation(){
+        switch(calculation){
+            case 1:
+                ans = num + Double.parseDouble(jTextField2.getText());
+                jTextField2.setText(Double.toString(ans));
+                break;
+                
+            case 2:
+                ans = num - Double.parseDouble(jTextField2.getText());
+                jTextField2.setText(Double.toString(ans));
+                break;
+                
+            case 3:
+                ans = num * Double.parseDouble(jTextField2.getText());
+                jTextField2.setText(Double.toString(ans));
+                break;
+                
+            case 4:
+                ans = num / Double.parseDouble(jTextField2.getText());
+                jTextField2.setText(Double.toString(ans));
+                break;
+        }
+    }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -47,6 +76,8 @@ public class Calculator extends javax.swing.JFrame {
         jButton16 = new javax.swing.JButton();
         jButton17 = new javax.swing.JButton();
         jButton19 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         jTextField1.setText("jTextField1");
 
@@ -198,6 +229,16 @@ public class Calculator extends javax.swing.JFrame {
 
         jButton19.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton19.setText("=");
+        jButton19.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton19ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setBackground(new java.awt.Color(255, 51, 153));
+
+        jLabel2.setForeground(new java.awt.Color(255, 51, 102));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -247,13 +288,22 @@ public class Calculator extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jButton14, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton15, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(jButton15, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(36, 36, 36)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -331,7 +381,10 @@ public class Calculator extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        jTextField2.setText(jTextField2.getText()+ "+");
+        num = Double.parseDouble(jTextField2.getText());
+        calculation = 2;
+        jTextField2.setText("");
+        jLabel2.setText(num + "-");
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
@@ -339,11 +392,17 @@ public class Calculator extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton11ActionPerformed
-        jTextField2.setText(jTextField2.getText()+ "*");
+        num = Double.parseDouble(jTextField2.getText());
+        calculation = 3;
+        jTextField2.setText("");
+        jLabel2.setText(num + "*");
     }//GEN-LAST:event_jButton11ActionPerformed
 
     private void jButton15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton15ActionPerformed
-        jTextField2.setText(jTextField2.getText()+ "/");
+        num = Double.parseDouble(jTextField2.getText());
+        calculation = 4;
+        jTextField2.setText("");
+        jLabel2.setText(num + "/");
     }//GEN-LAST:event_jButton15ActionPerformed
 
     private void jButton16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton16ActionPerformed
@@ -369,6 +428,11 @@ public class Calculator extends javax.swing.JFrame {
             jTextField2.setText(store);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton19ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton19ActionPerformed
+        arithimetic_operation();
+        jLabel2.setText("");
+    }//GEN-LAST:event_jButton19ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -425,6 +489,8 @@ public class Calculator extends javax.swing.JFrame {
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
     private javax.swing.JButton jButton9;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
