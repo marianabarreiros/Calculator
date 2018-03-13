212b23961ec747765def912ddfab4b757bb0bf57
Mariana Barreiros <maridepp1@hotmail.com>
2018-03-13 20:07:54 -0300

    criação do command e concrete command para fazer o padrão Command

---
+++ src/concretecommands/CommandSub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concretecommands;
+
+import interfaces.Command;
+
+/**
+ *
+ * @author aluno
+ */
+public class CommandSub implements Command {
+
+    @Override
+    public void redo() {
+        
+    }
+
+    @Override
+    public void undo() {
+        
+    }
+    
+}
